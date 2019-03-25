0279277c0a6a824d63e8c59316fe49b3aacdb34b
lidongyang <15650785172@163.com>
2019-03-25 12:22:03 +0800

    单元测试

--- src/app.js
+++ src/app.js
@@ -16,3 +16,67 @@ new Vue({
     }
 
 })
+
+import chai from 'chai'
+import spies from 'chai-spies'
+const expect = chai.expect
+chai.use(spies)
+
+//单元测试
+  {
+      const Constructor = Vue.extend(Button)
+      const vm = new Constructor({
+          propsData:{
+              icon:"shezhi"
+          }
+      })
+      vm.$mount()
+      let useElement = vm.$el.querySelector('use')
+      expect (useElement.getAttribute('xlink:href')).to.eq('#icon-shezhi')
+      vm.$el.remove()
+      vm.$destroy()
+  }
+  {
+      const Constructor = Vue.extend(Button)
+      const vm = new Constructor({
+          propsData:{
+              icon:'shezhi',
+              loading: true
+          }
+      })
+      vm.$mount()
+      let useElement = vm.$el.querySelector('use')
+      expect(useElement.getAttribute('xlink:href')).to.eq('#icon-loading')
+      vm.$el.remove()
+      vm.$destroy()
+  }
+  {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon:'shezhi',
+            iconPosition:'right'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let order = window.getComputedStyle(svg).order
+    expect(order).eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+  }
+  {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        icon:'shezhi'
+    })
+    vm.$mount()
+    let spy = chai.spy(function(){})
+    vm.$on('click',spy)
+
+    vm.$el.click()
+    expect(spy).to.have.been.called()
+
+  }
